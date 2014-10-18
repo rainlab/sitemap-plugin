03508831f15bb05440571d082b28a0c778b7d95f
Sam Georges <sam@daftspunk.com>
2014-10-18 16:09:57 +1100

    Add logic for generating sitemap XML

--- models/Definition.php
+++ models/Definition.php
@@ -1,12 +1,24 @@
 <?php namespace RainLab\Sitemap\Models;
 
+use URL;
 use Model;
+use DOMDocument;
 
 /**
  * Definition Model
  */
 class Definition extends Model
 {
+
+    /**
+     * Maximum URLs allowed (Protocol limit is 50k)
+     */
+    const MAX_URLS = 50000;
+
+    /**
+     * Maximum generated URLs per type
+     */
+    const MAX_GENERATED = 10000;
 
     /**
      * @var string The database table used by the model.
@@ -28,6 +40,11 @@ class Definition extends Model
      */
     protected $jsonable = ['data'];
 
+    /**
+     * @var integer A tally of URLs added to the sitemap
+     */
+    protected $urlCount = 0;
+
     public function beforeSave()
     {
         /*
@@ -47,4 +64,59 @@ class Definition extends Model
          */
         $this->setRawAttributes((array) $this->getAttributes() + (array) $this->data, true);
     }
+
+    public function generateSitemap()
+    {
+        // header("Content-Type: application/xml");
+        $xml = new DOMDocument;
+        $xml->encoding = 'UTF-8';
+
+        $urlset = $xml->createElement('urlset');
+        $urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+        $urlset->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $urlset->setAttribute('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
+
+        /*
+         * Cycle each page and add its URL
+         */
+        $pages = [];
+
+        foreach ($pages as $page) {
+
+            $pageUrl = URL::to('/');
+            $urlElement = $this->prepareUrlElement(
+                $xml,
+                $pageUrl,
+                date('c', $page->mtime),
+                'weekly',
+                '0.7'
+            );
+
+            if ($urlElement)
+                $urlset->appendChild($url);
+        }
+
+        /*
+         * @todo Cycle each registered definition type and add it
+         */
+
+        $xml->appendChild($urlset);
+        return $xml->saveXML();
+    }
+
+    protected function prepareUrlElement($xml, $pageUrl, $lastModified, $frequency, $priority)
+    {
+        if ($this->urlCount >= self::MAX_URLS)
+            return false;
+
+        $this->urlCount++;
+
+        $url = $xml->createElement('url');
+        $url->appendChild($xml->createElement('loc', $pageUrl));
+        $url->appendChild($xml->createElement('lastmod', $lastModified));
+        $url->appendChild($xml->createElement('changefreq', $frequency));
+        $url->appendChild($xml->createElement('priority', $priority));
+        return $url;
+    }
+
 }
