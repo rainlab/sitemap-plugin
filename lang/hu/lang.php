24ce37fb0c92e41eeb6f7d2d8fefcf83337d886a
gergo85 <gergo85@gmail.com>
2015-01-13 13:23:00 +0100

    Add Hungarian language

---
+++ lang/hu/lang.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Oldaltérkép',
+        'description' => 'A weboldalhoz sitemap.xml fájl generálása.'
+    ],
+    'item' => [
+        'location' => 'Hely:',
+        'priority' => 'Prioritás',
+        'changefreq' => 'Gyakoriság',
+        'always' => 'mindig',
+        'hourly' => 'óránkénti',
+        'daily' => 'napi',
+        'weekly' => 'heti',
+        'monthly' => 'havi',
+        'yearly' => 'évi',
+        'never' => 'soha',
+        'editor_title' => 'Módosítás',
+        'type' => 'Típus',
+        'allow_nested_items' => 'Engedélyezett elemek',
+        'allow_nested_items_comment' => 'Nested items could be generated dynamically by static page and some other item types',
+        'url' => 'URL',
+        'reference' => 'Hivatkozás',
+        'title_required' => 'A cím megadása kötelező',
+        'unknown_type' => 'Ismeretlen elem típus',
+        'unnamed' => 'Névtelen elem',
+        'add_item' => 'Hozzáadás',
+        'new_item' => 'Új elem',
+        'cms_page' => 'Oldal',
+        'cms_page_comment' => 'Select the page to use for the URL address.',
+        'reference_required' => 'A hivatkozó elem megadása kötelező.',
+        'url_required' => 'Az URL cím megadása kötelező',
+        'cms_page_required' => 'Válasszon egy oldalt',
+        'page' => 'Oldal',
+        'url' => 'URL',
+        'check' => 'Ellenőrzés'
+    ]
+];
