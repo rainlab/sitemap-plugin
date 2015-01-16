51af7756f17866a01506e3cdea45064f33124a01
jtherczeg <jtherczeg>
2015-01-16 17:50:29 +0100

    Hungarian language file merge request

---
+++ lang/hu/lang.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'plugin' => [
+         'name' => 'Oldaltérkép',
+        'description' => 'A webhely sitemap.xml fájljának generálása.'
+    ],
+    'item' => [
+        'location' => 'Hely:',
+        'priority' => 'Prioritás',
+        'changefreq' => 'Gyakoriság módosítása',
+        'always' => 'mindig',
+        'hourly' => 'óránként',
+        'daily' => 'naponta',
+        'weekly' => 'hetente',
+        'monthly' => 'havonta',
+        'yearly' => 'évente',
+        'never' => 'soha',
+        'editor_title' => 'Oldaltérképelem szerkesztése',
+        'type' => 'Típus',
+        'allow_nested_items' => 'Beágyazott elemek engedélyezése',
+        'allow_nested_items_comment' => 'A beágyazott elemeket statikus lap és néhány más elemtípus dinamikusan generálhatta',
+        'url' => 'URL-cím',
+        'reference' => 'Hivatkozás',
+        'title_required' => 'A Cím megadása kötelező',
+        'unknown_type' => 'Ismeretlen elemtípus',
+        'unnamed' => 'Névtelen elem',
+        'add_item' => '<u>E</u>lem hozzáadása',
+        'new_item' => 'Új elem',
+        'cms_page' => 'CMS-lap',
+        'cms_page_comment' => 'Válassza ki az URL-címhez használandó lapot.',
+        'reference_required' => 'Az elem hivatkozásának megadása kötelező.',
+        'url_required' => 'Az URL-cím megadása kötelező',
+        'cms_page_required' => 'Válasszon egy CMS-lapot',
+        'page' => 'Lap',
+        'url' => 'URL-cím',
+        'check' => 'Ellenőrzés'
+    ]
+];
