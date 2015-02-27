85f58ea093ba94ff863de8cdcd8a68ccd14b8b29
Talha Zekeriya Durmuş <drtzack@gmail.com>
2015-02-27 23:45:31 +0200

    Turkish Language Support

---
+++ lang/tr/lang.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    'plugin' => [
+        'name' => 'Site Haritası',
+        'description' => 'Web sitenize sitemap.xml dosyası oluşturur.'
+    ],
+    'item' => [
+        'location' => 'Konum:',
+        'priority' => 'Öncelik',
+        'changefreq' => 'Değişiklik Sıklığı',
+        'always' => 'sürekli',
+        'hourly' => 'saatlik',
+        'daily' => 'günlük',
+        'weekly' => 'haftalık',
+        'monthly' => 'aylık',
+        'yearly' => 'yıllık',
+        'never' => 'asla',
+        'editor_title' => 'Site haritası kaydını düzenle',
+        'type' => 'Tür',
+        'allow_nested_items' => 'İç içe kayıtlara izin ver',
+        'allow_nested_items_comment' => 'İç içe kayıtlar statik sayfalar ve bazı kayıtlar tarafından otomatik olarak oluşturulabilir.',
+        'url' => 'URL',
+        'reference' => 'Sayfa',
+        'title_required' => 'Başlık alanı gereklidir.',
+        'unknown_type' => 'Bilinmeyen kayıt türü',
+        'unnamed' => 'İsimsiz kayıt',
+        'add_item' => 'Yeni kayıt',
+        'new_item' => 'Yeni kayıt',
+        'cms_page' => 'CMS Sayfası',
+        'cms_page_comment' => 'Select the page to use for the URL address.',
+        'reference_required' => 'Kayıt sayfası alanı gereklidir.',
+        'url_required' => 'URL alanı gereklidir.',
+        'cms_page_required' => 'Lütfen bir CMS sayfası seçiniz.',
+        'page' => 'Sayfa',
+        'url' => 'URL',
+        'check' => 'Kontrol et'
+    ]
+];
