13a01ad34b340990a37cab612041912ffd4cdc31
Jose Aguado <jh2odo@gmail.com>
2015-03-30 16:35:06 +0200

    Fixed select active Theme and Added Spanish
    Fixed select active Theme
    Added Spanish language translations

---
+++ lang/es/lang.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Sitemap (mapa del sitio)',
+        'description' => 'Generar un fichero sitemap.xml para tu sitio web.'
+    ],
+    'item' => [
+        'location' => 'Localización',
+        'priority' => 'Prioridad',
+        'changefreq' => 'Frecuencia de cambio',
+        'always' => 'siempre',
+        'hourly' => 'A cada hora',
+        'daily' => 'Diariamente',
+        'weekly' => 'Semanalmente',
+        'monthly' => 'Mensualmente',
+        'yearly' => 'Anualmente',
+        'never' => 'nunca',
+        'editor_title' => 'Edita el elemento del Sitemap',
+        'type' => 'Tipo',
+        'allow_nested_items' => 'Permitir elemento anidados',
+        'allow_nested_items_comment' => 'Los elementos anidados podrán ser generados dinámicamente por páginas estáticas y otros tipos de elementos',
+        'url' => 'URL',
+        'reference' => 'Referencia',
+        'title_required' => 'El título es requerido',
+        'unknown_type' => 'Tipo de elemento desconocido',
+        'unnamed' => 'Elemento sin nombre',
+        'add_item' => 'Añadir <u>E</u>lemento',
+        'new_item' => 'Nuevo elemento',
+        'cms_page' => 'CMS Página',
+        'cms_page_comment' => 'Seleccione la página a usar para la dirección URL.',
+        'reference_required' => 'La referencia al elemento es requerida.',
+        'url_required' => 'La URL es requerida',
+        'cms_page_required' => 'Por favor seleccione una CMS página',
+        'page' => 'Página',
+        'url' => 'URL',
+        'check' => 'Verificar'
+    ]
+];
