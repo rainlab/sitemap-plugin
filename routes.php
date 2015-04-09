fd4ad173f3cb5b32252c46f5394d7c69f849177d
Samuel Georges <sam@daftspunk.com>
2015-04-10 07:57:58 +1000

    Minor tidy from #14

--- routes.php
+++ routes.php
@@ -1,12 +1,13 @@
 <?php
-use RainLab\Sitemap\Models\Definition;
+
 use Cms\Classes\Theme;
+use RainLab\Sitemap\Models\Definition;
 
 Route::get('sitemap.xml', function (){
-	
-	$themeActive = Theme::getActiveTheme()->getDirName();
 
-    return Response::make(Definition::where('theme','=',$themeActive)->firstOrFail()->generateSitemap())
+    $themeActive = Theme::getActiveTheme()->getDirName();
+
+    return Response::make(Definition::where('theme', $themeActive)->firstOrFail()->generateSitemap())
         ->header("Content-Type", "application/xml");
 
 });
