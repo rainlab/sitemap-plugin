9672b0692e340afbaf8bfc8900be454fd5f6625b
raank <raank92@gmail.com>
2015-10-30 09:25:22 -0200

    Added lang pt-br

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Sitemap',
+        'description' => 'Gera o arquivo sitemap.xml para seu site.',
+        'permissions' => [
+            'access_settings' => 'Acesso as definições de configuração so sitemap',
+        ],
+    ],
+    'item' => [
+        'location' => 'Localização:',
+        'priority' => 'Prioridade',
+        'changefreq' => 'Mudar a frequência',
+        'always' => 'sempre',
+        'hourly' => 'de hora em hora',
+        'daily' => 'diariamente',
+        'weekly' => 'semanalmente',
+        'monthly' => 'mensal',
+        'yearly' => 'anual',
+        'never' => 'nunca',
+        'editor_title' => 'Editar item no sitemap',
+        'type' => 'Tipo',
+        'allow_nested_items' => 'Permitir itens aninhados',
+        'allow_nested_items_comment' => 'Itens aninhados poderiam ser gerados dinamicamente por página estática e alguns outros tipos de itens',
+        'url' => 'URL',
+        'reference' => 'Referência',
+        'title_required' => 'O título é obrigatório',
+        'unknown_type' => 'Tipo de item Desconhecido',
+        'unnamed' => 'Sem nome',
+        'add_item' => 'Adicione um <u>I</u>tem',
+        'new_item' => 'Novo item',
+        'cms_page' => 'Página do CMS',
+        'cms_page_comment' => 'Selecione a página a ser usado para o endereço URL.',
+        'reference_required' => 'A referência é obrigatória.',
+        'url_required' => 'A URL é obrigatória.',
+        'cms_page_required' => 'Por favor, selecione uma página do CMS',
+        'page' => 'Página',
+        'url' => 'URL',
+        'check' => 'Checar'
+    ]
+];
